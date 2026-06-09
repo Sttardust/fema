fda2c8f387360d54402dac3ee5ff26ae3eef7367
fema education <leading.sapien@gmail.com>
2026-06-09 18:21:06 +0300

    Remove unused imports to unblock CI
    flutter analyze fails on warnings even with --no-fatal-infos.
    Four unused imports were promoted to warnings, blocking CI:
    - search_screen, library_screen, fema_intro_screen,
      onboarding_progress_header.
    
    All real dead code; safe to drop.

--- lib/features/onboarding/presentation/fema_intro_screen.dart
+++ lib/features/onboarding/presentation/fema_intro_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import '../domain/onboarding_provider.dart';
 import '../../../../core/constants/app_constants.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../../../core/theme/app_text_styles.dart';
